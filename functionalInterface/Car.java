42ad886fe42d556b037befaaacb3fca08a7fdddb
Hardhik <hardhikreddy1@gmail.com>
2024-11-06 12:18:42 +0530

    Lambda

---
+++ functionalInterface/Car.java
@@ -0,0 +1,11 @@
+package functionalInterface;
+@FunctionalInterface
+interface Greeting{
+    // void sayMessage();
+    void sayMessage(String s);
+}
+public class Car {
+    public static void main(String[] args) {
+        
+    }
+}

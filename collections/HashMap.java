c08c77cb1f51535714f3a528ed1c0a210853f0bb
Hardhik <hardhikreddy1@gmail.com>
2024-11-06 11:37:52 +0530

    second s commit

--- collections/HashMap.java
+++ collections/HashMap.java
@@ -2,6 +2,11 @@ package collections;
 import java.util.*;
 public class HashMap{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        HashMap<String, Integer> studentMarks = new HashMap<>();
+        studentMarks.put("Alice", 85);
+        studentMarks.put("Bob", 92);
+        studentMarks.put("Charlie", 78);
+        studentMarks.put("David", 88);
+        System.out.println("Initial HashMap: " + studentMarks);
     }
 }

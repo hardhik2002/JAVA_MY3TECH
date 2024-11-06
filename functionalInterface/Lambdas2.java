d1600e49853feb71de218ba2fcd2b3d6e3579f8c
Hardhik <hardhikreddy1@gmail.com>
2024-11-06 13:02:48 +0530

    Lambda

--- functionalInterface/Lambdas2.java
+++ functionalInterface/Lambdas2.java
@@ -5,19 +5,12 @@ interface Operation{
 }
 public class Lambdas2{
     public static void main(String[] args) {
-        Operation add=(int num1,int num2)->{
-            return num1+num2;
-        };
+        Operation add=(int num1,int num2)-> num1+num2;
         System.out.println(add.operate(10,20));
-        Operation sub=(int num1,int num2)->{
-            return num1-num2;
-        };
+        Operation sub=(int num1,int num2)-> num1-num2;
         System.out.println(sub.operate(10,5));
-        Operation mul=(int num1,int num2)->{
-            return num1*num2;
-        };
-        System.out.println(mul.operate(5,6));
-        
+        Operation mul=(int num1,int num2)-> num1*num2;
+        System.out.println(mul.operate(5,6));        
 
     }
 }

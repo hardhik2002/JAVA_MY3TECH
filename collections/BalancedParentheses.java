4519961a974f75ea911670e4eab591adba92584e
Hardhik <hardhikreddy1@gmail.com>
2024-11-08 14:51:07 +0530

    hashmap

--- collections/BalancedParentheses.java
+++ collections/BalancedParentheses.java
@@ -1,10 +1,10 @@
-package collections;
-import java.util.*;
-public class BalancedParentheses {
-    public static boolean isBalanced(String str){
-        Stack <Character> stack=new Stack<>();
-        for(char ch:str.toCharArray()){
+// package collections;
+// import java.util.*;
+// public class BalancedParentheses {
+//     public static boolean isBalanced(String str){
+//         Stack <Character> stack=new Stack<>();
+//         for(char ch:str.toCharArray()){
                 
-        }
-    }
-}
+//         }
+//     }
+// }
---
+++ collections/HashMap_.java
@@ -0,0 +1,10 @@
+package collections;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+public class HashMap_{
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+    }
+}

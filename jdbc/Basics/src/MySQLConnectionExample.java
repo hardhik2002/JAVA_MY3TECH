69be9313e6e0f5b8b88f6fc8951d47a717e43bbf
Hardhik <hardhikreddy1@gmail.com>
2024-11-08 11:58:50 +0530

    jdbc

--- jdbc/Basics/src/MySQLConnectionExample.java
+++ jdbc/Basics/src/MySQLConnectionExample.java
@@ -6,15 +6,17 @@ import java.sql.Statement;
 public class MySQLConnectionExample{
     public static void main(String[] args) {
         // Database URL, username, and password
-        String url = "jdbc:mysql://localhost:3306/customers";
+        String url = "jdbc:mysql://localhost:3306/customers?useSSL=false&serverTimezone=UTC";
         String username = "root";   // Replace with your MySQL username
         String password = "root";   // Replace with your MySQL password
-
+        //load the driver
+        
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
 
             // Execute a query
-            String query = "SELECT customers_id, firstname, email FROM customers";
+            String query = "SELECT customers_id, firstname,lastname,phone,address, email FROM customers";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
@@ -22,8 +24,11 @@ public class MySQLConnectionExample{
             while (resultSet.next()) {
                 System.out.println("ID: " + resultSet.getInt("customers_id"));
                 System.out.println("Name: " + resultSet.getString("firstname"));
+                System.out.println("LastName: " + resultSet.getString("lastname"));
                 System.out.println("Email: " + resultSet.getString("email"));
-                System.out.println("---------");
+                System.out.println("phone: " + resultSet.getString("Phone"));
+                System.out.println("address: " + resultSet.getString("address"));
+                System.out.println("--------------");
             }
 
             // Close resources
